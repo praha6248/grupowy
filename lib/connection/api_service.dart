712200b6e4e3c58cd9b9e8bddac3899024cf8089
Natalia Płocha <natalia.plocha6248@gmail.com>
2026-05-23 17:52:29 +0200

    fixing adress

--- lib/connection/api_service.dart
+++ lib/connection/api_service.dart
@@ -7,7 +7,7 @@ import 'package:socks5_proxy/socks_client.dart';
 class ApiService {
   late Dio _dio;
   final String baseUrl =
-      "http://eyfqp7vhlaxmo7adqdwz53golzfylabwzg6xoxqfpdve5g6xv6yvoyyd.onion";
+      "http://wgndjm6j2bxbluou33tnamlulusu7rrrtz2a7usho2y7s33bl6iqrayd.onion";
 
   ApiService() {
     _dio = Dio();
